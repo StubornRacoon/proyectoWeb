c494c8c6480b03ebd6a01ff858c526a96a38fe3b
Andrés Ramos <48600877+StubornRacoon@users.noreply.github.com>
2019-05-24 10:13:57 -0400

    TRIED SOMETHING
    AND SOMETHHING WORKED

--- js/code.js
+++ js/code.js
@@ -4,7 +4,7 @@ new Vue({
     el: '#app',
     data: {
         n:0,
-        recipe: [{step: "some text", image: "some url"}, {step: "some text1", image: "some url1"}, {step: "some text2", image: "some url3"}]
+        recipe: [{step: "some text", image: "https://static-news.moneycontrol.com/static-mcnews/2018/08/Business-Travel-1.jpg"}, {step: "some text1", image: "http://blog.edx.org/wp-content/uploads/2016/03/Business-Management.jpg"}, {step: "https://travel.state.gov/content/travel/en/us-visas/business/_jcr_content/tsg-rwd-content-page-parsysxxx/slideshow.img.png/1512148994695.png", image: "https://static-news.moneycontrol.com/static-mcnews/2018/08/Business-Travel-1.jpg"}]
       },
       methods: {
         previousStep: function(){
